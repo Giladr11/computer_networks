d787fcf424b166070dfb14409acc7ee363fa4676
Giladr11 <148324355+Giladr11@users.noreply.github.com>
2024-02-26 17:16:54 +0200

    scapy_improved_client

--- TCP_SCAPY_SERVER_CLIENT/client.py
+++ TCP_SCAPY_SERVER_CLIENT/client.py
@@ -5,12 +5,12 @@ import random
 
 
 EXIT_KEYWORD = "exit()"
-SYN_SEQ_NUMBER = random.randint(0, 4294967296)
-CLIENT_PACKETS_FILTER = "tcp and dst host {0} and dst port {1}"
+SYN_SEQ = random.randint(0, 4294967296)
 SYN_FLAG = 'S'
 ACK_FLAG = 'A'
-PUSH_FLAG = 'P'
 FIN_FLAG = 'F'
+INTERFACE = "\\Device\\NPF_{C357C829-E948-47FF-B0D2-608BDF16AFDC}"  # The Ethernet Interface
+CLIENT_PACKETS_FILTER = "tcp and src host %s and src port %s"
 
 
 def parameters_from_command_line():
@@ -23,61 +23,44 @@ def parameters_from_command_line():
     return args
 
 
-# A function that returns the "loopback" interface from the scapy iface list
-# The loopback iface is not connected to any device and used for testing purposes
-def get_iface():
-    interfaces = get_if_list()
-    loopback_iface = ""
-    for iface in interfaces:
-        if "loopback" in iface.lower():
-            loopback_iface = iface
-
-    return loopback_iface
-
-
 class Connections:
     def __init__(self):
         self.host = args.host
         self.port = args.port
-        self.packets_filter = CLIENT_PACKETS_FILTER.format(self.host, self.port)
-        self.interface_name = get_iface()
-        self.seq = SYN_SEQ_NUMBER
+        self.seq = SYN_SEQ
         self.ack = 0
+        self.iface = INTERFACE
+        self.filter = CLIENT_PACKETS_FILTER % (self.host, self.port)
 
     def connect(self):
         syn_packet = (IP(dst=self.host)
-                      / TCP(sport=self.port
-                            , dport=self.port
+                      / TCP(dport=self.port
                             , flags=SYN_FLAG
                             , seq=self.seq))
 
-        send(syn_packet)
+        print("syn_packet: (syn/ack)", syn_packet[TCP].seq, syn_packet[TCP].ack)
 
-        print("syn_packet (seq,ack) ", syn_packet.seq, syn_packet.ack)
+        syn_ack_packet = sr1(syn_packet, iface=self.iface)
 
-        syn_ack_packet = sniff(iface=self.interface_name, filter=self.packets_filter, count=1)[0]
+        print("syn_ack_packet: (syn/ack)", syn_ack_packet[TCP].seq, syn_ack_packet[TCP].ack)
 
-        print("syn_ack_packet (seq,ack) ", syn_ack_packet.seq, syn_ack_packet.ack)
-        print("syn_ack flag: ", syn_ack_packet[TCP].flags)
-
-        ack_packet = (IP(dst=syn_ack_packet[IP].src)
-                      / TCP(sport=syn_ack_packet[TCP].dport
-                            , dport=syn_ack_packet[TCP].sport
+        ack_packet = (IP(dst=self.host)
+                      / TCP(dport=self.port
                             , flags=ACK_FLAG
                             , seq=syn_ack_packet[TCP].ack
                             , ack=syn_ack_packet[TCP].seq + 1))
 
-        print("ack_packet (seq,ack) ", ack_packet.seq, ack_packet.ack)
+        print("ack_packet: (syn/ack)", ack_packet[TCP].seq, ack_packet[TCP].ack)
 
-        send(ack_packet)
-        response = sniff(iface=self.interface_name, filter=self.packets_filter, count=1)[0]
-
-        print("connection_msg_packet (seq,ack) ", response.seq, response.ack)
-        print("response flag: ", response[TCP].flags)
+        response = sr1(ack_packet, iface=self.iface)
 
         print(response[Raw].load.decode('utf-8'))
-        self.ack = response[TCP].seq + len(response[Raw].load)
-        self.seq = response[TCP].ack + 1
+
+        self.ack = response[TCP].seq
+        self.seq = response[TCP].ack
+
+        print("response seq: ", response[TCP].seq)
+        print("response ack: ", response[TCP].ack)
 
     def inc_seq(self, payload_size):
         self.seq += payload_size
@@ -85,21 +68,20 @@ class Connections:
     def inc_ack(self):
         self.ack += 1
 
-    def send_packet(self, load):
+    def send_packet(self, payload):
         data_packet = (IP(dst=self.host)
-                       / TCP(sport=self.port, dport=self.port
-                             , flags=(PUSH_FLAG+ACK_FLAG)
+                       / TCP(dport=self.port
                              , seq=self.seq
-                             , ack=self.ack)
-                       / Raw(load=load.encode('utf-8')))
+                             , ack=self.ack
+                             , flags='')
+                       / Raw(load=payload.encode('utf-8')))
 
-        send(data_packet)
+        send(data_packet, iface=self.iface)
 
         print("seq: ", self.seq)
         print("ack: ", self.ack)
 
         payload_size = len(data_packet[Raw].load)
-
         print("load: ", payload_size)
 
         self.inc_seq(payload_size)
@@ -108,13 +90,12 @@ class Connections:
     def close(self):
         print("\nSending a Fin Request To End The Connection...\n")
         fin_request = (IP(dst=self.host)
-                       / TCP(sport=self.port
-                             , dport=self.port
-                             , flags=(FIN_FLAG+ACK_FLAG)
+                       / TCP(dport=self.port
+                             , flags=FIN_FLAG+ACK_FLAG
                              , seq=self.seq
                              , ack=self.ack))
 
-        send(fin_request)
+        send(fin_request, iface=self.iface)
 
 
 class Client:
